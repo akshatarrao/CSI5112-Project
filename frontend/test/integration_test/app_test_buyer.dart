87995cf6b8fb48d4ffc903264818ec3497728d5c
Songyu-Wang <wangsongyuf@gmail.com>
2022-03-10 12:21:24 -0600

    This is a default message. I am too lazy to type anything

---
+++ frontend/test/integration_test/app_test_buyer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:flutter_login/src/widgets/animated_button.dart';
+import 'package:flutter_login/src/widgets/animated_text_form_field.dart';
+
+import 'package:csi5112_frontend/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('login in ',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      // We cannot test Login screen due to multiple issues:
+      // https://github.com/flutter/flutter/issues/31066
+      // https://github.com/NearHuscarl/flutter_login/issues/240
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text('Items List'), findsWidgets);
+    });
+  });
+}
