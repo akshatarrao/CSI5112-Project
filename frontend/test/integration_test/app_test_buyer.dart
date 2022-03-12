014ead8cad9205fee79b956507a854d6ec1928bb
Songyu-Wang <wangsongyuf@gmail.com>
2022-03-12 15:47:27 -0600

    This is a default message. I am too lazy to type anything

--- frontend/test/integration_test/app_test_buyer.dart
+++ frontend/test/integration_test/app_test_buyer.dart
@@ -10,38 +10,39 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('login in and log out',
+    testWidgets('all tests',
         (WidgetTester tester) async {
-      await setup(tester);
+       app.main();
+  await tester.pumpAndSettle();
+  // We cannot test Login screen due to multiple issues:
+  // https://github.com/flutter/flutter/issues/31066
+  // https://github.com/NearHuscarl/flutter_login/issues/240
+  await tester.tap(find.byType(ElevatedButton));
+  await tester.pumpAndSettle();
+
+      // Default landing test
       expect(find.text('Items List'), findsWidgets);
       await tester.tap(find.byType(AnimatedIcon));
       await tester.pumpAndSettle();
       await tester.tap(find.text("Log out"));
       await tester.pumpAndSettle();
       expect(find.text('LOGIN'), findsWidgets);
-    });
-    testWidgets('navigation',
-        (WidgetTester tester) async {
-      await setup(tester);
+        await tester.tap(find.byType(ElevatedButton));
+  await tester.pumpAndSettle();
+
+      // Side bar test
       await tester.tap(find.byType(AnimatedIcon));
       await tester.pumpAndSettle();
      expect(find.text('Discussion forum'), findsWidgets);
      expect(find.text('Order History'), findsWidgets);
-    });
-    testWidgets('navigation to discussion forum',
-        (WidgetTester tester) async {
-      await setup(tester);
-      await tester.tap(find.byType(AnimatedIcon));
-      await tester.pumpAndSettle();
+
+      // Nav to Discussion forum test
       await tester.tap(find.text("Discussion forum"));
       await tester.pumpAndSettle();
       expect(find.text('New'), findsOneWidget);
 
-    });
-    testWidgets('navigation to order history',
-        (WidgetTester tester) async {
-      await setup(tester);
-      await tester.tap(find.byType(AnimatedIcon));
+        // Nav to Order History test
+        await tester.tap(find.byType(AnimatedIcon));
       await tester.pumpAndSettle();
       await tester.tap(find.text("Order History"));
       await tester.pumpAndSettle();
@@ -49,15 +50,6 @@ void main() {
       expect(find.text('Search'), findsWidgets);
       expect(find.text('Order History'), findsWidgets);
     });
-  });
-}
 
-Future<void> setup(WidgetTester tester) async {
-   app.main();
-  await tester.pumpAndSettle();
-  // We cannot test Login screen due to multiple issues:
-  // https://github.com/flutter/flutter/issues/31066
-  // https://github.com/NearHuscarl/flutter_login/issues/240
-  await tester.tap(find.byType(ElevatedButton));
-  await tester.pumpAndSettle();
+  });
 }
