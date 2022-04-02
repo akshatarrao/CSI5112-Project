859f85aee10e3aed43a0902b92d3cfd8c7c2aaba
Andrew-Clappison <andrew.clappison@gmail.com>
2022-04-02 18:40:30 -0400

    Switch back to AWS backend for Questions & Answer

--- frontend/lib/util/constants.dart
+++ frontend/lib/util/constants.dart
@@ -4,5 +4,6 @@ class Constants {
   static const String titleTag = 'near.eGro.title';
   //static const String baseApi = "https://localhost:7156/api";
   static const String baseApi = "https://application.egrotech.net/api";
-  static const String baseApiDB = "https://localhost:7156/api";
+  //static const String baseApiDB = "https://localhost:7156/api";
+  static const String baseApiDB = "https://application.egrotech.net/api";
 }
