79aa2ce5e7ee13f9ff5400a6cb6d30cecc1346ce
Andrew-Clappison <andrew.clappison@gmail.com>
2022-04-05 13:33:33 -0400

    Removing Testing Bypass Login Button
    This button is for integration tests, was a mistake that it was left on in previous commit

--- frontend/lib/page/login_screen.dart
+++ frontend/lib/page/login_screen.dart
@@ -31,7 +31,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreentState extends State<LoginScreen> {
   Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
 
-  bool bypassLogin = true;
+  bool bypassLogin = false;
   bool bypassCustomer = true;
   late User currentUser;
 
